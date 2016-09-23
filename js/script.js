a50f6e85515ea087417b2afdc27266eac131881c
Vishnu Iyer <vishnu72498@gmail.com>
2016-09-22 22:36:56 -0700

    added images and awards

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("#mainbut").hover(function () {
+            $(this).css("opacity", "1.0");
+    });
+    $("#mainbut").mouseleave(function () {
+            $(this).css("opacity", "0.7");
+    });
+    $("#aboutbut").hover(function () {
+            $(this).css("opacity", "1.0");
+    });
+    $("#aboutbut").mouseleave(function () {
+            $(this).css("opacity", "0.7");
+    });
+    $("#educationbut").hover(function () {
+            $(this).css("opacity", "1.0");
+    });
+    $("#educationbut").mouseleave(function () {
+            $(this).css("opacity", "0.7");
+    });
+    $("#skillbut").hover(function () {
+            $(this).css("opacity", "1.0");
+    });
+    $("#skillbut").mouseleave(function () {
+            $(this).css("opacity", "0.7");
+    });
+    $("#honorbut").hover(function () {
+            $(this).css("opacity", "1.0");
+    });
+    $("#honorbut").mouseleave(function () {
+            $(this).css("opacity", "0.7");
+    });
+    $("#projectbut").hover(function () {
+            $(this).css("opacity", "1.0");
+    });
+    $("#projectbut").mouseleave(function () {
+            $(this).css("opacity", "0.7");
+    });
+        
+});
+
+function moveTo (location) {
+    
+}
