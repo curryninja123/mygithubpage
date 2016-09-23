9591f5f57e5cc646b4844be60c66e8599a9da9ed
Vishnu Iyer <vishnu72498@gmail.com>
2016-09-23 00:04:52 -0700

    finished honors page

--- js/script.js
+++ js/script.js
@@ -35,9 +35,40 @@ $(document).ready(function(){
     $("#projectbut").mouseleave(function () {
             $(this).css("opacity", "0.7");
     });
+    $("#mainbut").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#main").offset().top
+        }, 1000); 
+    });
+    $("#aboutbut").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000); 
+    });
+    $("#educationbut").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#education").offset().top
+        }, 1000); 
+    });
+    $("#skillbut").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 1000); 
+    });
+    $("#honorbut").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#honors").offset().top
+        }, 1000); 
+    });
+    $("#projectbut").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 1000); 
+    });
         
 });
 
-function moveTo (location) {
-    
+
+function eduScroll(tab) {
+    $("#"+tab).slideToggle();
 }
